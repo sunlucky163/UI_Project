33286a710f45064aae96de7f74fae00a69b76a22
sunlucky <mishushin93@gmail.com>
2022-06-29 17:07:16 +0400

    fix component.ts

--- __ui__/src/components/component.ts
+++ __ui__/src/components/component.ts
@@ -2,11 +2,11 @@ import { Page } from '@Core/page';
 import { Element } from '@Core/element';
 
 export class Component {
-    protected page: Page;
-    protected element: Element;
+  protected page: Page;
+  protected element: Element;
 
-    constructor(page: Page, element: Element | null = null) {
-        this.page = page;
-        this.element = element;
-    }
+  constructor(page: Page, element: Element | null = null) {
+    this.page = page;
+    this.element = element;
+  }
 }
