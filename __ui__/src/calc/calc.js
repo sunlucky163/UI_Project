bd7ac6f33c7631ebdede1071b2e1e6d40d2b8c5d
sunlucky <mishushin93@gmail.com>
2022-06-29 17:56:30 +0400

    lesson

---
+++ __ui__/src/calc/calc.js
@@ -0,0 +1,14 @@
+export class Calc {
+  // eslint-disable-next-line prettier/prettier
+  public PI = 3.14;
+  private PO = 'slovo';
+
+  public sort(array) {
+    return array.sort()
+  }
+}
+
+export function getName(name) {
+  return name
+
+}
