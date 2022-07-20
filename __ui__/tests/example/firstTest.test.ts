cdc5349030e0157d2a88374652a03a709300646c
Ivan <z85z@mail.ru>
2022-07-20 07:19:31 +0300

    Homework Lessson 8. Lamoda.ru

--- __ui__/tests/example/firstTest.test.ts
+++ __ui__/tests/example/firstTest.test.ts
@@ -2,6 +2,7 @@ import { HomePage } from '@Components/home/homePage';
 import { Browser } from '@Core/browser';
 import { Page } from '@Core/page';
 import { timeout } from '@Utils/timeout';
+import { shouldInstrument } from '@jest/transform';
 
 describe('First test', () => {
   const browser = new Browser();
@@ -20,13 +21,17 @@ describe('First test', () => {
   //need create test with lamoda with use method click for elements (3-5)
   test('should be', async () => {
     await homePage.open();
-    await page.getByText('');
-    const first = await page.$('');
-    await first.click();
-    await first.attributes();
-    await first.click();
-    const second = await page.$('selector');
-    await second.click();
+    await timeout(5000);
+    const kids = await page.$$('.wCjUeog4KtWw64IplV1e6');
+    // console.log(text1);
+    await kids[8].click();
+    await timeout(8000);
+    const man = await page.$$('.wCjUeog4KtWw64IplV1e6');
+    console.log(man);
+    await man[7].click();
+    await timeout(8000);
+    const woman = await page.$$('.wCjUeog4KtWw64IplV1e6');
+    await woman[6].click();
     await timeout(100000);
   });
 });
